2479b3d5db123927c31f516c1c3fe6fcd6f5c94b
Emeka Victor <devemmy001@gmail.com>
2026-09-15 15:20:07 +0100

    removed title badge

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
       <div className="max-w-7xl mx-auto px-4 md:px-6 flex flex-col items-center text-center">
 
         {/* 1. Status Badge */}
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -27,7 +27,7 @@ export default function Hero() {
             <span className="relative inline-flex rounded-full h-2 w-2 bg-primary"></span>
           </span>
           Building from Aba, Nigeria
-        </motion.div>
+        </motion.div> */}
 
         {/* 2. Main Headline */}
         <motion.h1
